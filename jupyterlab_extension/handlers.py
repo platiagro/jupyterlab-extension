97fa2c76b51e87c0fea48dc2d40360ca9e712cb3
Miguel Ferraz <miguel.f.ferraz.27@gmail.com>
2020-10-21 16:27:44 -0300

    Debugging test

--- jupyterlab_extension/handlers.py
+++ jupyterlab_extension/handlers.py
@@ -39,6 +39,7 @@ class RouteHandler(APIHandler):
                 self.set_status(200)
                 self.set_header("Content-Type", "application/json")
                 data = json.dumps(local_file, default=date_default)
+                print("New file uploaded.")
                 self.finish(data)
             else:
                 self.set_status(500)
